459504789c62d9c319fe34a52300572fef5d365e
boukhemis11 <boukhemis11@gmail.com>
2018-11-17 14:34:53 +0100

    add skills

--- Angular/my-app/src/app/addskill/addskill.component.ts
+++ Angular/my-app/src/app/addskill/addskill.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { SIGKILL } from 'constants';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+
 
 @Component({
   selector: 'app-addskill',
@@ -8,16 +10,27 @@ import { SIGKILL } from 'constants';
 })
 export class AddskillComponent implements OnInit {
 
-  name:String 
-  phone:String
-  skill:String
-  adress:String
-  salaire:String
-  remarque:String
 
-  constructor() { }
+  data = {
+    name: '',
+    phone: '',
+    skill: '',
+    adress: '',
+    salaire: '',
+    remarque: ''
+};
+
+itemList: AngularFireList<any>;
+
+  constructor(db: AngularFireDatabase) {
+    this.itemList = db.list('skills');
+   }
 
   ngOnInit() {
+  }
+
+  insertSkill() {
+    this.itemList.push(this.data);
   }
 
 }
